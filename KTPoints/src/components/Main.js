ddd8c111ee69002d2a302cd3423b7087de359418
Edward_Knighton <edwardknighton@edwardkhtonsmbp.guest.uc.edu>
2019-03-03 02:30:15 -0500

    Calling API

--- KTPoints/src/components/Main.js
+++ KTPoints/src/components/Main.js
@@ -1,10 +1,54 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import Points from './Points'
 import Header from './Header'
 import { createStackNavigator,createAppContainer, DrawerNavigator } from 'react-navigation';
 
+
 class Main extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            major: '',
+            meetingsLeft: '',
+            points: '',
+            standing: '',
+            name: '',
+            year: '',
+            pc: '',
+            url: ''
+
+        }
+    }
+
+    componentWillMount() {
+        url = 'https://firestore.googleapis.com/v1/projects/ktpoints-68071/databases/(default)/documents/users/';
+        apiCall = url.concat(this.props.navigation.state.params.uniqname);
+        this.setState({url: apiCall});
+        console.log(this.state.url);
+        fetch(apiCall)
+        .then((response) => {
+            if (!response.ok) throw Error(response.statusText);
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+            console.log("Getting here")
+            this.setState({
+                major: data['fields']['major']['stringValue'],
+                meetingsLeft: data['fields']['meetings_left']['stringValue'],
+                points: data['fields']['points']['integerValue'],
+                standing: data['fields']['standing']['stringValue'],
+                name: data['fields']['name']['stringValue'],
+                year: data['fields']['year']['stringValue'],
+                pc: data['fields']['pledge_class']['stringValue']
+
+            })
+            console.log(this.state)
+          });
+    }
+
+
     static navigationOptions = {
         title: 'Home',
         headerStyle: {
@@ -17,7 +61,7 @@ class Main extends React.Component {
   render(){
     return (
         <View>
-            <Header HeaderText={'Testing'} navigation={this.props.navigation}/>
+            <Header HeaderText={this.state.name} navigation={this.props.navigation}/>
             <Points/>
         </View>
     );
