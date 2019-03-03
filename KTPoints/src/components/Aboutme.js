8194ad5cb028be5d5e2d232bbf1fb0ca4b9eb6bd
mayakh <mayakh@umich.edu>
2019-03-03 04:49:56 -0500

    About me most recent

--- KTPoints/src/components/Aboutme.js
+++ KTPoints/src/components/Aboutme.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View, StyleSheet,Image } from 'react-native';
+import { View, StyleSheet,Image,Text } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 import axios from 'axios'
+import Header from './Header';
 
 
 class AboutMe extends Component {
@@ -11,6 +12,7 @@ class AboutMe extends Component {
         UserID:'mayakhanna4',
         First_Name: 'Maya',
         Last_Name: 'Khanna',
+        name:'Maya Khanna',
         Brotherhood_Standing: 'Active',
         Major: 'Computer Science Engineering',
         Class_Standing: 'Sophmore',
@@ -20,21 +22,46 @@ class AboutMe extends Component {
         Img_url:'',
 
     }
-    componentWillMount(){
-        axios.get('https://epss.ucla.edu/media/images/profile_pictures/default.jpg')
-        .then(response => this.setState({Img_url: response.data}));
-    }
+    // componentWillMount(){
+    //     axios.get('https://epss.ucla.edu/media/images/profile_pictures/default.jpg')
+    //     .then(response => this.setState({Img_url: response.data}));
+    // }
     
     render() {
         
         return (
             <View>
+                <Header HeaderText={this.state.name} />
                 <Card>
                     <CardSection>
                     <Image
-                       style={{width: 200, height: 200}}
-                       source={{uri: this.state.Img_url}}
+                       style={{width: 100, height: 200}}
+                       source={{uri: 'https://scontent.fluk1-1.fna.fbcdn.net/v/t1.0-9/32453143_1208787859257914_2538505098489757696_n.jpg?_nc_cat=106&_nc_ht=scontent.fluk1-1.fna&oh=680a71abc19864319b03248f849461eb&oe=5D236440'}}
                        />
+                       <Text>
+                        <Text style = {styles.text_style}>Major: {this.state.Major} 
+                        </Text>
+                        {'\n'}
+                        <Text style = {styles.text_style}>Grade: {this.state.Class_Standing} 
+                        </Text>
+                        {'\n'}
+                        <Text style = {styles.text_style}>Standing: {this.state.Brotherhood_Standing}
+                        </Text>
+                        {'\n'}
+                        <Text style = {styles.text_style}> Pledge Class: {this.state.Pledge_Class}
+                        </Text>
+                        {'\n'}
+                       </Text>
+                    </CardSection>
+                </Card>
+                <Card>
+                    <CardSection>
+                        <Text>
+                        <Text style = {styles.text_style}>Current Points: {this.state.Current_Points}</Text>
+                        {'\n'}
+                        <Text style = {styles.text_style}>Meetings Left: {this.state.Meetings_Left}</Text>
+                        {'\n'}
+                       </Text>
                     </CardSection>
                 </Card>
                 <Card>
@@ -59,7 +86,12 @@ const styles = StyleSheet.create({
     loginButton:{
       backgroundColor:  '#2980b6',
        color: '#fff'
-    }
+    },
+    text_style:{
+         fontSize: 24,
+         textAlign: 'center',
+         color: '#000'
+      }
 });
 
 export default AboutMe;
